0fe7d373bf3873ce9431ecbcabd1c8124ccd7b34
Pedro Cardoso <135392261+paulocardoso8377@users.noreply.github.com>
2023-07-26 19:53:33 -0300

    atualização

---
+++ aula03/aula03.js
@@ -0,0 +1,25 @@
+"use strict"
+
+"use strict"
+/*
+function teste (){
+
+if(true){
+   let nome = "Clara"
+    console.log("Dentro do if do teste:" + nome)
+
+}
+
+console.log("Dentro de teste:" + nome)
+
+}
+
+teste()
+
+console.log("Dentro de teste:" + nome)
+   */
+
+const curso = "curso javascript"
+curso = "HTML"
+console.log(curso)
+
