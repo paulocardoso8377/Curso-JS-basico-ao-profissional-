a213cffafc5e78b7cc6c9adee080f298d91bbd31
Pedro123445678 <135392261+paulocardoso8377@users.noreply.github.com>
2023-08-03 19:24:50 -0300

    atualização de  js

---
+++ aula11/aula11.js
@@ -0,0 +1,20 @@
+function segurança(){
+
+console.log("Voce esta ronpendo os padrões do sistema!!!!")
+
+}
+
+
+for(i=0;i<15;i++){
+    if(i<10) 
+    console.log(i + " Ainda está no limite...")
+    
+
+else{
+   console.log("Voce ultrapassou o limite" + segurança())
+   
+
+}       
+}  
+
+
---
+++ aula12/aula12.js
@@ -0,0 +1,11 @@
+const obj = document.getElementsByTagName('div')
+
+let num= [10,20,30,40,50]
+
+for(o of obj){
+    console.log(o.innerHTML = "SUUUPER CURSO")
+}
+
+for(o in obj){
+    console.log(o)
+}
