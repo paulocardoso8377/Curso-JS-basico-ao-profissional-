d09d83e95fd11a06c56731d709ab4b9cb671c559
Pedro123445678 <135392261+paulocardoso8377@users.noreply.github.com>
2023-08-12 21:14:47 -0300

    atualização

---
+++ aula18/aula18.js
@@ -0,0 +1,10 @@
+function soma(...valores){
+    let tam = valores.length
+   let res = 0
+    for(i=0;i<tam;i++){
+        res +=valores[i]
+       
+    }
+     return 
+}
+console.log(soma(10,5,2,8))
