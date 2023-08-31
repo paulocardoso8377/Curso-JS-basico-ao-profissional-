c010066310e9bbcf259d8f494a8a754bd59613c2
Pedro123445678 <135392261+paulocardoso8377@users.noreply.github.com>
2023-08-31 18:55:28 -0300

    atualização js

--- aula25/aula25.js
+++ aula25/aula25.js
@@ -1,11 +1,3 @@
-const cursostodos = [...document.getElementsByClassName("curso")]; // Correção aqui
-const cursosC1 = [...document.getElementsByClassName("c1")]
-const cursosC2 = [...document.getElementsByClassName("c2")]
-
-console.log(cursostodos);
-console.log(cursosC1);
-console.log(cursosC2);
-
-cursostodos.map((el) => {
-    el.classList.add("destaque")
-});
+const  msg =()=>{
+    alert("clicou")
+}
